01e1fe2d2f11b048293561b57b269b22b4cebfdc
ArtNowak <nowak.artr@gmail.com>
2022-04-18 21:41:56 +0200

    Auth user test

--- spec/system/authenticate_user_system_spec.rb
+++ spec/system/authenticate_user_system_spec.rb
@@ -1,16 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe 'Sign up', type: :feature do
-  before do
-    @employee = FactoryBot.build(:employee)
-  end
+  let(:employee) { build(:employee) }
+  let(:created_employee) { create(:employee) }
 
   it 'signs up' do
     visit '/employees/sign_up'
     within('form') do
-      fill_in 'Email', with: @employee.email
-      fill_in 'Password', with: @employee.password
-      fill_in 'Password confirmation', with: @employee.password
+      fill_in 'Email', with: employee.email
+      fill_in 'Password', with: employee.password
+      fill_in 'Password confirmation', with: employee.password
     end
     click_button 'Sign up'
     expect(page).to have_content 'You have signed up successfully'
@@ -18,11 +17,11 @@ RSpec.describe 'Sign up', type: :feature do
   end
 
   it 'signs in' do
-    @employee = FactoryBot.create(:employee)
+    # @employee = create(:employee)
     visit '/employees/sign_in'
     within('form') do
-      fill_in 'Email', with: @employee.email
-      fill_in 'Password', with: @employee.password
+      fill_in 'Email', with: created_employee.email
+      fill_in 'Password', with: created_employee.password
     end
     click_button 'Log in'
     expect(page).to have_content 'Signed in successfully'
